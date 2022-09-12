4a04707264ed24fb412164a24cb5af49b95262a8
Kenneth Presedo <kennethpresedo@live.com>
2022-09-11 22:49:36 -0400

    game  boards made and intro fixed

--- app.js
+++ app.js
@@ -3,8 +3,9 @@
 ////////////////////////////////////////////////////////////////////////////////
 const startEl = document.getElementById('start-btn-container');
 const modalEl = document.getElementById('modal');
-const startButtoJim = document.getElementById('jim');
+const startButtonJim = document.getElementById('jim');
 const startButtonDwight = document.getElementById('dwight');
+const h1El = document.getElementById('select');
 
 
 
@@ -17,11 +18,37 @@ const startButtonDwight = document.getElementById('dwight');
 const openModal = () => {
     startEl.style.display = 'none';
     modalEl.style.visibility = 'visible';
-    console.log('worked')
+    
 }
 
+const instructionsJim = () => {
+    startButtonDwight.style.display = 'none';
+    h1El.innerHTML = ``;
+    modalEl.style.height = '250px';
+    modalEl.innerHTML = `
+    <h2>Jim Halpert</h2>
+     <h4>Born and raised in Scranton, Jim enjoys cycling, basketball, playing ping pong, eating soft-shell crab, and playing pranks on his friends, family and co-workers.</h4>
+    `;
 
+}
 
+const instructionsDwight = () => {
+    startButtonJim.style.display = 'none';
+    h1El.innerHTML = ``;
+    modalEl.style.height = '250px';
+    modalEl.innerHTML = `
+    <h2>Dwight Schrute</h2>
+    <h4>Dwight Schrute began his career at Dunder Mifflin as a salesperson, but quickly rose through the ranks to assume the title of Assistant to the Regional Manager.</h4>
+    `
+
+}
+
+// const nextSlide = () => {
+//     modalEl.innerHTML = '';
+//     modalEl.innerHTML = `
+//     <h4>Instructions</h4>
+//     `;
+// }
 
 
 
@@ -33,4 +60,7 @@ const openModal = () => {
 //     console.log('image clicked')
 // })
 
-startEl.addEventListener('click', openModal)
+startEl.addEventListener('click', openModal);
+startButtonJim.addEventListener('click', instructionsJim);
+startButtonDwight.addEventListener('click', instructionsDwight);
+// modalEl.addEventListener('click', nextSlide);
