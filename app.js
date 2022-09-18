acdf91d544932313723ff40baa0b83722bd42034
Kenneth Presedo <kennethpresedo@live.com>
2022-09-18 19:03:39 -0400

    working on logic

--- app.js
+++ app.js
@@ -101,12 +101,14 @@ const jimSelectionB = () => {
 };
 
 const dwightSelectionA = () => {
-    dwightTrackerEl.innerHTML = 'klk'
-};
+
+}
 
 const dwightSelectionB = () => {
-    dwightTrackerEl.innerHTML = 'klk'
+
 };
+
+
 
 
 /////////////////////////////////////////////////////////////////////////////////
@@ -127,6 +129,11 @@ choiceJimAbuttonEl.addEventListener('click', jimSelectionA);
 choiceJimBbuttonEl.addEventListener('click', jimSelectionB);
 choiceDwightAbuttonEl.addEventListener('click', dwightSelectionA);
 choiceDwightBbuttonEl.addEventListener('click', dwightSelectionB);
+
+
+/////////////////////////////////////////////////////////////////////////////////
+//Game Logic      ///////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
 
 const dwightsAdventures = {
     starting: 'What color shirt is Dwight wearing today?',
@@ -178,12 +185,115 @@ const dwightsAdventures = {
     spicyBrown: {
         q: 'You have been selected to be the saftey officer or Dunder Mifflin Scranton, should you start a fire in sakes of teaching fire saftey in the office?',
         noSafetyMoment: {
-
+            q: 'Good call, that wouldve been bad, however Micheal just burned his foot on his George Forman Grill whats your next move?',
+            saveMicheal: {
+                q: 'Attempting to rush him to the hospital, you have crashed into a post, your car is damaged, and your head isnt feeling that well, whats next',
+                goToHospital: {
+                    q: 'The doctor says you have a concussion,',
+                    goHome: {
+                        result: 'you blew it, youll never become ARM!',
+                    },
+                    continueWorking: {
+                        result: 'You incorrectly shipped out 1500 reems of paper instead of 150, youre fired, youll never become ARM!',
+                    },
+                },
+                dontGoToHospital: {
+                    q: 'your vision is blurred and you cant see straight',
+                    result: 'You incorrectly shipped out 1500 reems of paper instead of 150, youre fired, youll never become ARM!',
+                },
+            },
+            dontSaveMicheal: {
+                q: 'Micheal is upset nobody helped him with his burned foot, how can you make it up to him?',
+                mintChoclateChipIceCream: {
+                    result: 'Congratulations!!! Youre now the offical ARM',
+                },
+                meatBallParmSub: {
+                    result: 'its Alfrados worst sub, you did this to yourself, youll never become ARM',
+                },
+            },
         },
         yesSafteyMoment: {
-
+            q: 'beause of your demonstration, you caused fellow salesman Stanley Hudson to have a heart attack at work',
+            issuePublicAppology: {
+                q: 'This time evryone has forgiven you, do we push our luck or step down as saftey officer',
+                forfeitSafteyOfficer: {
+                    q: 'because youre on such this ice, what can you do to smooth things over',
+                    buyEveryoneBeakfast: {
+                        result: 'You made no advance in becoming ARM',
+                    },
+                    meetWithJanBehindMichealsBack: {
+                        result: 'You have been fired for going behind Micheals back, its over, youll never become ARM',
+                    },
+                },
+                planBombScare: {
+                    result: 'You have been fired, youll never become ARM',
+                },
+            },
+            dontAppologize: {
+                q: 'You have been given a second chance, but youre on thin ice',
+                meetWithAngela: {
+                    q: 'meet at the warehouse on company time or after work at home?',
+                    meetAtWarehouse: {
+                        result: 'You have been caught by a warehouse worker, youre fired and will never become ARM',
+                    },
+                    meetAtHome: {   
+                        result: 'You made no advance in becoming ARM',
+                    },
+                },
+                sellPaper: {
+                    result: 'You locked in a huge client from a large company, you made Dunder Mifflin $1M, welcome to management, congratulations Dwight!'
+                },
+            },
         },
-
-    },
+    }
 },
 }
+
+// 
+// else (dwightSelectionB) {
+//     dwightTrackerEl.innerHTML = 'You have been selected to be the saftey officer or Dunder Mifflin Scranton, should you start a fire in sakes of teaching fire saftey in the office?';
+//     choiceDwightAbuttonEl.innerHTML = 'Teach the entire office a lesson at fire saftey by pulling the fire alarm and shouting instructions on how to exit the building';
+//     choiceDwightBbuttonEl.innerHTML = 'Dont make an example with a fake fire.';
+// };
+// }
+// }
+
+
+//     // }else{
+//     //     dwightTrackerEl.innerHTML = 'You have been selected to be the saftey officer or Dunder Mifflin Scranton, should you start a fire in sakes of teaching fire saftey in the office?';
+//     //     if (dwightSelectionB) {
+//     //         choiceDwightAbuttonEl.innerHTML = 'Teach the entire office a lesson at fire saftey by pulling the fire alarm and shouting instructions on how to exit the building';
+//     //         choiceDwightBbuttonEl.innerHTML = 'Dont make an example with a fake fire.';
+//     //     };
+      
+//     }
+// if (dwightSelectionA){
+//  
+//     
+//    }
+//    if(dwightSelectionB){
+
+//    }
+// };
+// }
+
+// if (dwightSelectionA){
+//     dwightTrackerEl.innerHTML = 'Your coworkers are now upset with you for picking such a barebones insurance plan, they all mde you feel guilty';
+//     choiceDwightAbuttonEl.innerHTML = 'Quit and go work at Staples';
+//     choiceDwightBbuttonEl.innerHTML = 'Make it up to everyone?';
+// }
+
+// if (dwightSelectionA){
+//     dwightTrackerEl.innerHTML = 'Micheal has put you in charge of selecting an insurance plan for the office. You can cut costs and make Micheals boss, Jan, happy but also upsetting your colleuges or pick a sweet plan and make your coworkers happy but uposet Jan';
+//     choiceDwightAbuttonEl.innerHTML = 'Pick a low budget plan that cuts costs but upset evryone';
+//     choiceDwightBbuttonEl.innerHTML = 'Pick a high end plan thats expensive for the company but makes coworkers happy';
+// }
+//     if (dwightSelectionA){
+//         dwightTrackerEl.innerHTML = 'Your coworkers are now upset with you for picking such a barebones insurance plan, they all mde you feel guilty';
+//         choiceDwightAbuttonEl.innerHTML = 'Quit and go work at Staples';
+//         choiceDwightBbuttonEl.innerHTML = 'Make it up to everyone?';
+//    } else {
+//     dwightTrackerEl.innerHTML = 'All of your coworkers are happy with you, but now Micheal is upset with you because Jan is yelling at him for not cutting costs';
+//     choiceDwightAbuttonEl.innerHTML = 'You can ask for a preformance review';
+//     choiceDwightBbuttonEl.innerHTML = 'You can meet with Jan (Micheals boss) behind his back to say you can run the branch better than Micheal';
+//    };
